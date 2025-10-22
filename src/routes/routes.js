cc9c64b5daf4e0cf9a765720d8d305e51003e22b
Victor Oluwayemi <oluwayemivictor15@gmail.com>
2025-10-22 06:09:10 +0100

    add more routes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,15 +1,18 @@
 import express from 'express';
 import submitStringsController from '../controllers/submitStringsController.js';
 import getSpecificStringController from "../controllers/getSpecificStringController.js";
-import getAllStringsController from "../controllers/getAllStringsController.js"
+import getAllStringsController from "../controllers/getAllStringsController.js";
+import deleteStringController from "../controllers/deleteStringController.js";
+// import getStringsWithNaturalLanguageController from "../controllers/getStringsWithNaturalLanguageController.js";
+
 
 const router = express.Router();
 
 router.post('/strings', submitStringsController);
-router.get('/strings/{id}', getSpecificStringController);
 router.get('/strings/all', getAllStringsController);
+router.get('/strings/:id', getSpecificStringController);
 // router.get('/strings', getStringsWithNaturalLanguageController);
 // router.get('/strings/filter-by-natural-language', getStringsWithNaturalLanguageController);
-// router.delete('/strings/{id}', deleteStringController);
+router.delete('/strings/:id', deleteStringController);
 
 export default router;
